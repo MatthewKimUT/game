8a1ddd99f9f847e5c3a5207aed7ceb8cc070b124
Matt <>
2018-04-29 08:26:29 -0500

    Added image files and figured out how to display them

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,13 @@
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
-import javax.swing.JFrame;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import javax.imageio.ImageIO;
+import javax.swing.*;
 
 public class Game extends JFrame{
     public static void main(String[] args){
@@ -18,6 +24,8 @@ public class Game extends JFrame{
     int windowWidth = 400;
     int ballX = 0;
     int ballY = 50;
+    ArrayList<BufferedImage> heroSprites;
+    ImageTypes images;
 
     public void run(){
         initialize();
@@ -46,12 +54,12 @@ public class Game extends JFrame{
     }
 
     void initialize(){
-         backBuffer = new BufferedImage(windowWidth, windowHeight, BufferedImage.TYPE_INT_RGB);
+        backBuffer = new BufferedImage(windowWidth, windowHeight, BufferedImage.TYPE_INT_RGB);
         input = new InputHandler(this);
         insets = getInsets();
         setSize(insets.left + windowWidth + insets.right,
                 insets.top + windowHeight + insets.bottom);
-
+         //splitSprites("\"resources/graphics/tiny-rpg-town-files/tileset/grass-tile.png\"");
         setTitle("Game Tutorial");
         setSize(windowWidth, windowHeight);
         setResizable(false);
@@ -75,11 +83,29 @@ public class Game extends JFrame{
 
         Graphics bbg = backBuffer.getGraphics();
 
+//        String iconPath = "../resources/graphics/tiny-rpg-town-files/tileset/grass-tile.png";
+//        ImageIcon hero = null;
+//        URL url = getClass().getClassLoader().getResource(iconPath);
+//        if(url != null)
+//            hero = new ImageIcon(url);
+//        final Image img = hero.getImage();
         bbg.setColor(Color.WHITE);
         bbg.fillRect(0, 0, windowWidth, windowHeight);
 
-        bbg.setColor(Color.BLACK);
-        bbg.drawOval(ballX, ballY, 20, 20);
-        g.drawImage(backBuffer, insets.left, insets.top, this);
+        try{
+            BufferedImage image = ImageIO.read(new File("resources/graphics/tiny-rpg-town-files/tileset/grass-tile.png"));
+
+            bbg.setColor(Color.BLACK);
+            //bbg.drawOval(ballX, ballY, 20, 20);
+            bbg.drawImage(image, ballX, ballY, 20, 20, null);
+            g.drawImage(backBuffer, insets.left, insets.top, this);
+        }
+        catch(Exception e){
+            System.out.println("Exception thrown: " + e.getLocalizedMessage());
+        }
     }
+
+//    ArrayList<BufferedImage> splitSprites(String path){
+//
+//    }
 }
---
+++ src/ImageTypes.java
@@ -0,0 +1,9 @@
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+public class ImageTypes {
+    private ArrayList<BufferedImage> heroWalkingLeft;
+    private ArrayList<BufferedImage> heroWalkingRight;
+    private ArrayList<BufferedImage> heroWalkingUp;
+    private ArrayList<BufferedImage> heroWalkingDown;
+}
